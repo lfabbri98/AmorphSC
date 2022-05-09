a146cd0a27fb0550c9535d85b8e7769329832560
lfabbri98 <79851576+lfabbri98@users.noreply.github.com>
2022-05-09 17:39:19 +0200

    Added function to calculate lamp spectrum

--- photocurrent.py
+++ photocurrent.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
 
-def calculate_PC(data, gain, change_positions):
+def calculate_PC(data, gain, change_positions, lamp=False):
     """
     Function that given input data returns the photocurrent spectra calculated
     with each 
@@ -31,3 +33,35 @@ def calculate_PC(data, gain, change_positions):
     #Now concatenate all the spectra to a single one
     Amp = np.stack(Amps)
     return Amp
+
+def lamp_spec(data):
+    """
+    Function that takes into account a set of lamp data and returns the adjustment
+    of the set with the sensitivity of photodiode
+
+    Parameters
+    ----------
+    data : fdataframe
+        dataframe recorded by the instrument
+
+    Returns
+    -------
+    floar
+        vector with data adjusted with sensitivity
+
+    """
+    r = 0.63 #cm
+    A = 3.1415 * r * r
+    L1=[]
+    pdc = pd.read_csv("SensitivityPhotodiode.dat", sep="\t", names=["WL", "S"])
+    pdc_int = interp1d(pdc.WL, pdc.S)
+    sens_new_WL = np.linspace(200,1000, 1000 )
+    sens_new = pdc_int(sens_new_WL)
+    for i in range(len(data.WL)):
+        for j in range(len(sens_new_WL)):
+            if (data.WL[i] >= sens_new_WL[j] and data.WL[i] < sens_new_WL[j+1]):
+                L1.append(data.Amp[i] /( 1e-3 * sens_new[j]))
+                break
+            else: continue
+    L1 = np.asarray(L1)
+    return L1/A
