753f730591b933056f79eecdedbb51d342beaac8
lfabbri98 <79851576+lfabbri98@users.noreply.github.com>
2022-05-09 18:22:25 +0200

    Now photocurrent works for calculate_PC

--- photocurrent.py
+++ photocurrent.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 
-def calculate_PC(data, gain, change_positions, lamp=False):
+def calculate_PC(data, gain, change_positions):
     """
     Function that given input data returns the photocurrent spectra calculated
     with each 
@@ -23,15 +23,14 @@ def calculate_PC(data, gain, change_positions, lamp=False):
 
     """
     
-    Amps = [] #vector of amplitudes
+    Amp = []#vector of amplitudes
     sens = data.s[0] #sensitivity 
     p = 0 # counter for initial position
     for i, j in enumerate(change_positions):
         amp = data.Amp[p:j+1] #take that part of spectrum
         amp = amp*sens*1e-4 /gain[i] #adjust with gain and sens
-        Amps.append(amp) #append
-    #Now concatenate all the spectra to a single one
-    Amp = np.stack(Amps)
+        p = j+1
+        Amp = np.concatenate((Amp, amp))
     return Amp
 
 def lamp_spec(data):
