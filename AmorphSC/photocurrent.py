1e748328d928d137f550246c546aa3bb88c5130a
lfabbri98 <79851576+lfabbri98@users.noreply.github.com>
2022-05-22 18:17:38 +0200

    Updated functions with data type and relative docstrings

--- AmorphSC/photocurrent.py
+++ AmorphSC/photocurrent.py
@@ -2,18 +2,20 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 
-def calculate_PC(data, gain, change_positions):
+def calculate_PC(amplitude: list, sensitivity: float, gain: list, change_positions: list) -> list:
     """
     Function that given input data returns the photocurrent spectra calculated
     with each 
 
     Parameters
     ----------
-    data : 
+    amplitude : list of float
         input data of photocurrent
-    gain : float
+    gain : list of float
         vector with gains used
-    change_positions : float
+    sensitivity : float
+        sensitivity of lockin
+    change_positions : list of float
         vector with positions at which the gain has been changed
 
     Returns
@@ -24,25 +26,36 @@ def calculate_PC(data, gain, change_positions):
     """
     
     Amp = []#vector of amplitudes
-    sens = data.s[0] #sensitivity 
+    sens = sensitivity #sensitivity 
     p = 0 # counter for initial position
     for i, j in enumerate(change_positions):
-        amp = data.Amp[p:j+1] #take that part of spectrum
+        amp = amplitude[p:j+1] #take that part of spectrum
         amp = amp*sens*1e-4 /gain[i] #adjust with gain and sens
         p = j+1
         Amp = np.concatenate((Amp, amp))
     return Amp
 
-def lamp_spec(data, gain, change, diode_area):
+def lamp_spec(amplitude: list, wavelength: list, sensitivity: float, gain: list, change:list , diode_area: float) -> list:
     """
     Function that takes into account a set of lamp data and returns the adjustment
     of the set with the sensitivity of photodiode
 
     Parameters
     ----------
-    data : fdataframe
-        dataframe recorded by the instrument
-
+    
+    amplitude : list of float
+        values of lamp amplitude   
+    wavelength : list of float
+        values of acquired wavelength
+    sensitivity : float
+        lockin sensitivity
+    gain : list of float
+        vector with gains used
+    change : list of float
+        vector with positions at which the gain has been changed
+    
+    
+    
     Returns
     -------
     floar
@@ -53,17 +66,21 @@ def lamp_spec(data, gain, change, diode_area):
     L1=[]
     
     #import photodiode sensitivities
-    pdc = pd.read_csv("SensitivityPhotodiode.dat", sep="\t", names=["WL", "S"])
+    try:
+        pdc = pd.read_csv("SensitivityPhotodiode.dat", sep="\t", names=["WL", "S"])
+    except:
+        print("You must have the file with diode sensitivity in the folder!")
+        exit()
     #perform and interpolation to smooth conversion
     pdc_int = interp1d(pdc.WL, pdc.S)
     sens_new_WL = np.linspace(200,1000, 1000 )
     sens_new = pdc_int(sens_new_WL)
     #calculate spectrum of lamp
-    PC = calculate_PC(data, gain, change)
+    PC = calculate_PC(amplitude, sensitivity, gain, change)
     #now multiply sensitivity
-    for i in range(len(data.WL)):
+    for i in range(len(wavelength)):
         for j in range(len(sens_new_WL)):
-            if (data.WL[i] >= sens_new_WL[j] and data.WL[i] < sens_new_WL[j+1]):
+            if (wavelength[i] >= sens_new_WL[j] and wavelength[i] < sens_new_WL[j+1]):
                 L1.append(PC[i] /( 1e-3 * sens_new[j]))
                 break
             else: continue
